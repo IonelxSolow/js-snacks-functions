e03a2df4ec054e42f8746113bd930dde1a71d57b
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-17 16:38:42 +0100

    add function for 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,31 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function contaVocali (str)  {
+    let vocali = "aeiouAEIOU";
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vocali.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(contaVocali(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+/* const contaVocali = (str) => {
+    let vocali = "aeiouAEIOU";
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vocali.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+} */

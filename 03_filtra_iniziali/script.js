634b644d94406a75ed750844607f68d064b670ed
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-17 16:25:03 +0100

    add function for filter name by letter

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filterByLetter(nameArray, letter) {
+    let filteredArray = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        if (nameArray[i].charAt(0) === letter) {
+            filteredArray.push(nameArray[i])
+        }
+    }
+    return filteredArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filterByLetter(names, 'A'))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+
+/* const filterByLetter = (nameArray, letter) => nameArray.filter(name => name[0] === letter); */
